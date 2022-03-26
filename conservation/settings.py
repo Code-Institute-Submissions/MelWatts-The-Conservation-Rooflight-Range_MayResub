3c0630da4eab789383b5e2ec40e3c5cf5c4def69
Mel Watts <mel.watts@therooflightcompany.co.uk>
2022-03-26 13:01:07 +0000

    mend heroku app name in settings

--- conservation/settings.py
+++ conservation/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['rooflight-company.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['rooflight-company.herokuapp.com', 'localhost']
 
 
 # Application definition
